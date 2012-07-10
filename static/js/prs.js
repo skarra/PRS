de1a662fd243b90a0d05d20d7011423b1bc75bc9
Sriram Karra <karra.etc@gmail.com>
2012-07-10 07:39:43 +0530

    Add a js text input filtering mechanism to exclude some chars, say

--- static/js/prs.js
+++ static/js/prs.js
@@ -1,6 +1,6 @@
 //
 // Created       : Sat May 05 13:15:20 IST 2012
-// Last Modified : Fri Jul 06 15:18:26 IST 2012
+// Last Modified : Tue Jul 10 07:31:09 IST 2012
 //
 // Copyright (C) 2012, Sriram Karra <karra.etc@gmail.com>
 // All Rights Reserved
@@ -341,6 +341,106 @@ function addHandlers_department_edit() {
     });    
 }
 
+// The following function addTextFilters() is taken verbatim from
+// example 17.06 of David Flanagan's book "Javascript - The Definitive
+// Guide" His own desription and comments are reproduced in full below
+
+/*
+ * InputFilter.js: unobtrusive filtering of keystrokes for <input> elements
+ *
+ * This module finds all <input type="text"> elements in the document that
+ * have an "data-allowed-chars" attribute. It registers keypress, textInput, and
+ * textinput event handlers for any such element to restrict the user's input
+ * so that only characters that appear in the value of the attribute may be
+ * entered. If the <input> element also has an attribute named "data-messageid",
+ * the value of that attribute is taken to be the id of another document
+ * element. If the user types a character that is not allowed, the message
+ * element is made visible. If the user types a character that is allowed, the
+ * message element is hidden. This message id element is intended to offer
+ * an explanation to the user of why her keystroke was rejected. It should
+ * typically be styled with CSS so that it is initially invisible.
+ *
+ * Here is sample HTML that uses this module.
+ *   Zipcode: <input id="zip" type="text"
+ *                   data-allowed-chars="0123456789" data-messageid="zipwarn">
+ *   <span id="zipwarn" style="color:red;visibility:hidden">Digits only</span>
+ *
+ * This module is purely unobtrusive: it does not define any symbols in
+ * the global namespace.
+ */
+function addTextFilters () {
+    // Find all <input> elements
+    var inputelts = document.getElementsByTagName("input");
+    // Loop through them all
+    for(var i = 0 ; i < inputelts.length; i++) {
+        var elt = inputelts[i];
+        // Skip those that aren't text fields or that don't have
+        // a data-allowed-chars attribute.
+        if (elt.type != "text" || !elt.getAttribute("data-allowed-chars"))
+            continue;
+        
+        // Register our event handler function on this input element
+        // keypress is a legacy event handler that works everywhere.
+        // textInput (mixed-case) is supported by Safari and Chrome in 2010.
+        // textinput (lowercase) is the version in the DOM Level 3 Events draft.
+        if (elt.addEventListener) {
+            elt.addEventListener("keypress", filter, false);
+            elt.addEventListener("textInput", filter, false);
+            elt.addEventListener("textinput", filter, false);
+        }
+        else { // textinput not supported versions of IE w/o addEventListener()
+            elt.attachEvent("onkeypress", filter); 
+        }
+    }
+
+    // This is the keypress and textInput handler that filters the user's input
+    function filter(event) {
+        // Get the event object and the target element target
+        var e = event || window.event;         // Standard or IE model
+        var target = e.target || e.srcElement; // Standard or IE model
+        var text = null;                       // The text that was entered
+
+        // Get the character or text that was entered
+        if (e.type === "textinput" || e.type === "textInput") text = e.data;
+        else {  // This was a legacy keypress event
+            // Firefox uses charCode for printable key press events
+            var code = e.charCode || e.keyCode;
+
+            // If this keystroke is a function key of any kind, do not filter it
+            if (code < 32 ||           // ASCII control character
+                e.charCode == 0 ||     // Function key (Firefox only)
+                e.ctrlKey || e.altKey) // Modifier key held down
+                return;                // Don't filter this event
+
+            // Convert character code into a string
+            var text = String.fromCharCode(code);
+        }
+        
+        // Now look up information we need from this input element
+        var allowed = target.getAttribute("data-allowed-chars"); // Legal chars
+        var messageid = target.getAttribute("data-messageid");   // Message id
+        if (messageid)  // If there is a message id, get the element
+            var messageElement = document.getElementById(messageid);
+        
+        // Loop through the characters of the input text
+        for(var i = 0; i < text.length; i++) {
+            var c = text.charAt(i);
+            if (allowed.indexOf(c) == -1) { // Is this a disallowed character?
+                // Display the message element, if there is one
+                if (messageElement) messageElement.style.visibility = "visible";
+
+                // Cancel the default action so the text isn't inserted
+                if (e.preventDefault) e.preventDefault();
+                if (e.returnValue) e.returnValue = false;
+                return false;
+            }
+        }
+
+        // If all the characters were legal, hide the message if there is one.
+        if (messageElement) messageElement.style.visibility = "hidden";
+    }
+}
+
 function addHandlers () {
     console.log('addFormHandlers...');
 
@@ -434,6 +534,8 @@ function addHandlers () {
     addHandlers_doctor_view();
     addHandlers_doctor_edit();
     addHandlers_department_edit()
+
+    addTextFilters();
 }
 
 function onLoad () {
